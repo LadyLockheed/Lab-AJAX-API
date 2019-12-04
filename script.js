77d03f20c14d02fdfca3f86b2bc6824c7e80e96b
Karin Karlsson <karin.drake@gmail.com>
2019-12-04 09:27:14 +0100

    function for add book

--- script.js
+++ script.js
@@ -25,19 +25,85 @@ keyFrame.innerHTML = data.key; // nyckeln skrivs ut på konsolen
 
 
 
+
+
+
+
+
 });
 
 
 
+//Karins kod för att lägga till ett bokobjekt-------------
+
+
+let buttonAddBook=document.querySelector(".add-Books-Button");
+let bookList=document.querySelector(".book-List");
+let inputTitle=document.querySelector("#input-title");
+let inputAuthor=document.querySelector("#input-author");
+let body=document.querySelector("body");
 
 
 
+buttonAddBook.addEventListener("click", function() {
+    console.log("Knappen i add book funkar")
+    createBook();
+
+
+})
 
 
 
+function createNewDivImage(){
+    
+    let newDivImage=document.createElement("div");
+    newDivImage.className="book-image";
+    return newDivImage;
+
+}
 
 
+function createNewDivTitle(){
 
+    let newDivTitle=document.createElement("div");
+    newDivTitle.className="book-title";
+    newDivTitle.innerText="Title: "+inputTitle.value;
+    return newDivTitle;
+}
+
+function createNewDivAuthor(){
+    
+    let newDivAuthor=document.createElement("div");
+    newDivAuthor.className="book-author";
+    newDivAuthor.innerText="Author: "+inputAuthor.value;
+    return newDivAuthor;    
+}
+
+//Den här funktionen skapar hela boken inkl alla tre element som ligger i och appendar den till book-list
+function createBook(){
+    let bookDiv=document.createElement("div")
+    bookDiv.className="book";
+    let imageElem=createNewDivImage();
+    let titleElem = createNewDivTitle();
+    let authorElem=createNewDivAuthor();
+    //här skapas alla de tre elementen som ska ligga i 
+    bookDiv.appendChild(imageElem);
+    bookDiv.appendChild(titleElem);
+    bookDiv.appendChild(authorElem);
+    
+    bookList.appendChild(bookDiv);
+  
+}
+
+
+// function createBookList(){
+
+//     let parentToBooks=document.createElement("div");
+//     parentToBooks.className="book-List";
+//     body.appendChild(parentToBooks);
+
+    
+// }
 
 
 
